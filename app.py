db14879dd068c38d3014f6bbbdad4d1f6e14a9c1
Marcos Jesus <marcos.jesus@weduu.com>
2023-11-01 16:10:33 -0300

    Upload de arquivos + conlusão da rota usuarios com niveis de acesso

---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, jsonify
+from flask_restful import Api
+from resources.hotel import Hoteis, Hotel
+from resources.usuario import UserList, UserRegister, UserLogin, UserLogout
+from resources.site import Site, Sites
+from flask_jwt_extended import JWTManager
+from blocklist import BLOCKLIST
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///banco.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'DontTellAnyone'
+app.config['JWT_BLOCKLIST_ENABLED'] = True
+api = Api(app)
+jwt = JWTManager(app)
+
+
+@app.before_request
+def cria_banco():
+    banco.create_all()
+
+
+@jwt.token_in_blocklist_loader
+def verifica_blocklist(self,token):
+    return token['jti'] in BLOCKLIST
+
+
+@jwt.revoked_token_loader
+def access_token_invalidation(jwt_header, jwt_payload):
+    return jsonify(msg=f"You have been logged out!"), 401
+
+#rota hoteis
+api.add_resource(Hoteis, '/hoteis')
+api.add_resource(Hotel, '/hoteis/<string:hotel_id>')
+#rota usuarios
+api.add_resource(UserList, '/usuarios','/usuarios/<int:user_id>')
+#rota cadastro
+api.add_resource(UserRegister, '/cadastro')
+#rota usuarios
+api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+#rota sites
+api.add_resource(Sites, '/sites')
+api.add_resource(Site, '/sites/<string:url>')
+
+if __name__ == '__main__':
+    from sql_alchemy import banco
+    banco.init_app(app)
+    app.run(debug=True)
---
+++ resources/usuario.py
@@ -0,0 +1,97 @@
+from flask_restful import Api, Resource, reqparse
+from models.usuario import UserModel
+from flask_jwt_extended import create_access_token, jwt_required, get_jwt, get_jwt_identity
+from secrets import compare_digest
+from blocklist import BLOCKLIST
+from functools import wraps
+from flask import request
+
+
+atributos_registro = reqparse.RequestParser()
+atributos_registro.add_argument('login', type=str, required=True, help="The field 'login' cannot be blank")
+atributos_registro.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be blank")
+atributos_registro.add_argument('tipo_usuario', type=int, required=True, help="The field 'tipo_usuario' cannot be blank")
+
+atributos_login = reqparse.RequestParser()
+atributos_login.add_argument('login', type=str, required=True, help="The field 'login' cannot be blank")
+atributos_login.add_argument('senha', type=str, required=True, help="The field 'senha' cannot be blank")
+
+
+def role_required(*roles):
+    def wrapper(fn):
+        @wraps(fn)
+        def decorated_function(*args, **kwargs):
+            current_user_id = get_jwt_identity()
+            user = UserModel.find_user(current_user_id)
+            
+            if user.tipo_usuario in roles:
+                return fn(*args, **kwargs)
+            else:
+                return {'message': 'Unauthorized access for this user type'}, 403
+        return decorated_function
+    return wrapper
+
+
+#Classe do elemento
+class UserList(Resource):
+    @jwt_required()
+    def get(self, user_id=None):
+        if user_id:
+            user = UserModel.find_user(user_id)
+            if user:
+                return user.json()
+            return {'message': 'User not found'}, 404
+        else:
+            users = UserModel.obter_todos_usuarios()
+            users_list = [user.json() for user in users]
+            return {'users': users_list}, 200
+
+    @jwt_required()
+    @role_required(2,3)
+    def delete(self, user_id):
+        user = UserModel.find_user(user_id)
+        if user:
+            try:
+                user.delete_user()
+                return {'message': "User with ID {} is deleted".format(user_id)}, 200
+            except:
+                return {'message': 'An internal error occurred trying to delete User'}, 500
+        return {'message': 'User not found'}, 404
+
+
+class UserRegister(Resource):
+    def post(self):
+        dados = atributos_registro.parse_args()
+
+        if UserModel.find_by_login(dados['login']):
+            return {"message": "Login '{}' already exists".format(dados['login'])}, 400
+
+        # Verifique se o tipo de usuário é um valor válido (1, 2 ou 3)
+        if dados['tipo_usuario'] not in [1, 2, 3]:
+            return {"message": "Invalid tipo_usuario. It should be 1 for user, 2 for customer, or 3 for admin"}, 400
+
+        user = UserModel(login=dados['login'], senha=dados['senha'], tipo_usuario=dados['tipo_usuario'])
+        user.save_user()
+        return {'message': 'User {} created successfully!'.format(dados['login'])}, 201
+
+
+class UserLogin(Resource):
+    @classmethod
+    def post(cls):
+        dados = atributos_login.parse_args()
+        user = UserModel.find_by_login(dados['login'])
+        if user and compare_digest(user.senha, dados['senha']):
+            token_de_acesso = create_access_token(identity=user.user_id)
+            return {'access_token': token_de_acesso}, 200
+        return {'message': 'The username or password is incorrect.'}, 401
+
+
+class UserLogout(Resource):
+    @jwt_required()
+    def post(self):
+        jwt_id = get_jwt()['jti']
+        BLOCKLIST.add(jwt_id)
+        return {'message': "logged out successfully"},200
+
+
+
