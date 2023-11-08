969c321615b10dc8c80c0a22ec5e0a05cda812b0
Marcos Jesus <marcos.jesus@weduu.com>
2023-11-08 16:43:31 -0300

    Adicionado controle de acesso de usuario nas demais rotas

--- resources/site.py
+++ resources/site.py
@@ -1,5 +1,12 @@
 from flask_restful import Resource
 from models.site import SiteModel
+from flask_jwt_extended import jwt_required
+from resources.user_level import role_required
+
+from resources.normalize import normalize_path_params
+
+normalize_path_params()
+
 
 class Sites(Resource):
     def get(self):
@@ -12,6 +19,7 @@ class Site(Resource):
             return site.json()
         return {'message':'Site not found.'}, 404
     
+    @jwt_required()
     def post(self, url):
         if SiteModel.find_site(url):
             return{'message':'An site with this URL already exists'}, 400
@@ -22,6 +30,8 @@ class Site(Resource):
             return{"message":"Error while creating the site."},500
         return site.json()
     
+    @jwt_required()
+    @role_required(2,3)
     def delete(self, url):
         site = SiteModel.find_site(url)
         if site:
